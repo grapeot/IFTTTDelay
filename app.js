7ba05d3f248cdd764cf4cf91fd918107e78dc148
Yan Wang <grapeot@gmail.com>
2016-10-04 10:14:52 -0700

    Initial import.

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('//delay', function(req, res) {
+    var delay = parseInt(req.query.t); // in minutes
+    var key = req.query.key;
+    var event = req.query.event;
+    setTimeout(function() {
+        var url = 'https://maker.ifttt.com/trigger/' + event + '/with/key/' + key;
+        console.log('URL = ' + url);
+        request.post(url, function(error, response, body) {
+        });
+    }, delay * 60 * 1000);
+    res.send('Request recorded. Delay = ' + delay + ' minutes, event = ' + event + ', key = ' + key);
+});
+
+app.use('/', function(req, res) {
+    res.status(200).json({"status": "ok"});
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
